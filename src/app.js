d65eb047947c6ed78a48e9b822600d54b0f5c7f4
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-27 15:47:25 +0300

    set up DB connection and server start

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express');
+const errorHandlingMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+const connectDB = require('./db/connect');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello there!');
+});
+
+app.use(errorHandlingMiddleware);
+app.use(notFoundMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
