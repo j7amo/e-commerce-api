6b563b1a31e2bc4281b7e51fa0251ffc08c94c52
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-28 17:53:40 +0300

    update User controllers and routes

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,23 +1,33 @@
+const { StatusCodes } = require('http-status-codes');
 const User = require('../models/User');
+const CustomError = require('../errors/index');
 
 const getAllUsers = async (req, res) => {
-  res.send('get all users');
+  const users = await User.find({ role: 'user' }).select('-password');
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send('get single user');
+  const { id: userId } = req.params;
+  const user = await User.findOne({ _id: userId }).select('-password');
+
+  if (!user) {
+    throw new CustomError.NotFoundError(`User with id ${userId} not found!`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
-  res.send('show current user');
+  const user = await User.findOne({});
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const updateUser = async (req, res) => {
-  res.send('update user');
+  res.json(req.body.name);
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send('update user password');
+  res.json(req.body.password);
 };
 
 module.exports = {
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -6,13 +6,14 @@ const {
   updateUser,
   updateUserPassword,
 } = require('../controllers/user-controller');
+const authenticateUser = require('../middleware/authentication');
 
 const router = express.Router();
 
-router.route('/').get(getAllUsers);
-router.route('/showMe').get(showCurrentUser);
-router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/').get(authenticateUser, getAllUsers);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
+router.route('/updateUser').patch(authenticateUser, updateUser);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 // Keep in mind that the ORDER here is IMPORTANT:
 // if we put any of the previous 3 routes
 // below the "/:id" then we will not be able to access those resources.
