3e30c6d1c31f38b21d20a692bf475792041e370e
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-28 09:27:21 +0300

    add pre-hook and comparePasswords method for use on User instance

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validate = require('validator');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +30,15 @@ const UserSchema = new mongoose.Schema({
     default: 'user',
   },
 });
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePasswords = async function (passwordCandidate) {
+  const isMatch = await bcrypt.compare(passwordCandidate, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model('User', UserSchema);
